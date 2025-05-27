94d515fb8eaf2a140bcda08f367548d380b8a99e
Kim Yongbeom <ghzx55@naver.com>
2025-05-27 12:09:30 +0900

    [feature] karpenter 관련 .sh 주석 처리 (#2)

--- step8.sh
+++ step8.sh
@@ -1,11 +1,11 @@
-terraform apply --auto-approve \
-  -target=aws_iam_role.karpenter \
-  -target=aws_iam_role_policy_attachment.karpenter_extra \
-  -target=aws_iam_instance_profile.karpenter \
-  -target=aws_iam_role.karpenter_controller_role \
-  -target=aws_iam_policy.karpenter_controller_policy \
-  -target=aws_iam_role_policy_attachment.karpenter_policy_attach \
-  -target=helm_release.karpenter \
-  -target=null_resource.wait_for_karpenter \
-  -target=kubectl_manifest.karpenter_node_class \
-  -target=kubectl_manifest.karpenter_node_pool
+# terraform apply --auto-approve \
+#   -target=aws_iam_role.karpenter \
+#   -target=aws_iam_role_policy_attachment.karpenter_extra \
+#   -target=aws_iam_instance_profile.karpenter \
+#   -target=aws_iam_role.karpenter_controller_role \
+#   -target=aws_iam_policy.karpenter_controller_policy \
+#   -target=aws_iam_role_policy_attachment.karpenter_policy_attach \
+#   -target=helm_release.karpenter \
+#   -target=null_resource.wait_for_karpenter \
+#   -target=kubectl_manifest.karpenter_node_class \
+#   -target=kubectl_manifest.karpenter_node_pool
