0f8adc5ed7c0bc213643ca9cd7c73525ddb03eb3
ghzx55 <ghzx55@naver.com>
2025-05-27 22:21:51 +0900

    [Feature] EKS 및 패키지 설치 자동화 워크플로우 추가

--- step3.sh
+++ step3.sh
@@ -1,1 +1,9 @@
 terraform apply --auto-approve -target=aws_eks_cluster.this
+
+terraform import aws_iam_role.cluster daenamu-test-eks-cluster-role || true
+terraform import aws_iam_role.node_group daenamu-test-node-group-role || true
+terraform import aws_iam_instance_profile.node_group daenamu-test-node-group-instance-profile || true
+terraform import aws_iam_role.alb_sa_role AmazonEKSLoadBalancerControllerRole || true
+terraform import aws_iam_policy.alb_policy arn:aws:iam::${AWS_ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy || true
+
+
