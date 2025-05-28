3510095fec6e4517de9ceebfc34f938fbeb087af
ghzx55 <ghzx55@naver.com>
2025-05-28 22:11:41 +0900

    [feature] step9.sh에 nginx 정의

--- step9.sh
+++ step9.sh
@@ -1,4 +1,7 @@
 terraform apply --auto-approve \
   -target=kubernetes_deployment.daenamu_api \
   -target=kubernetes_service.daenamu_api \
-  -target=kubernetes_ingress_v1.daenamu_api_ingress
+  -target=kubernetes_ingress_v1.daenamu_api_ingress \
+  -target=kubernetes_deployment.nginx \
+  -target=kubernetes_service.nginx \
+  -target=kubernetes_ingress_v1.nginx_ingress \
